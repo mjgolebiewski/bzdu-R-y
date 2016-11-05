8c154f6f7d55038ac83fce7df7a81f9f3cfa3b43
mjgolebiewski <sheepthedruid@gmail.com>
2016-11-05 12:13:37 +0100

    nic specjalnego, move on

--- coursera_notatki_04_11.R
+++ coursera_notatki_04_11.R
@@ -1,13 +1,16 @@
+install.packages('jsonlite')
+library(jsonlite)
+
 # xd <- rnorm(100)
 
 # write.csv(xd, file = 'dane_testowe.csv')
 # eksport do pliku csv
 
-# dput(xd, file = 'dput_test.txt')
+# dput(xd, file = 'randomowe_dane/dput_test.txt')
 # zapisanie pliku jako pliku tekstowego z metadanymi; posiada min. informacje o typach
 # danych w kolumnach; podobno lepiej wspopracuje z systemami kontrolii wersji
 
-dx <- dget(file = 'dput_test.txt')
+dx <- dget(file = 'randomowe_dane/dput_test.txt')
 # odczytanie danych zapisanych dput
 
 xd <- read.csv('dane_testowe.csv')
@@ -18,3 +21,13 @@ sapply(xd, class)
 
 head(xd)
 str(xd)
+
+url.show(url = 'http://python-data.dr-chuck.net/known_by_Fikret.html')
+conurl <- url('http://python-data.dr-chuck.net/known_by_Fikret.html', 'r')
+x <- readLines(conurl)
+head(x)
+
+con <- file(description = 'randomowe_dane/dput_test.txt', 'r')
+zz <- readLines(con, n = 10)
+zz
+
